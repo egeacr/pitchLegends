5a9ae452467b558766c5e8a700c76bf3b7c11cb4
onurgokmenkoch <163167466+onurgokmenkoch@users.noreply.github.com>
2024-10-06 22:37:55 +0100

    FOE-27/Generated-playpage-and-few-locators

--- pages/playPage/playPage.ts
+++ pages/playPage/playPage.ts
@@ -1,12 +1,13 @@
 import { Locator, Page } from "@playwright/test";
 import BasePage from "../basePage/basePage";
 
-export class HomePage extends BasePage {
+export class PlayPage extends BasePage {
 
     readonly page: Page
     private firstPlayForFreeButton: Locator
     private secondPlayForFreeButton: Locator
     private randomNameButton: Locator
+    private confirmButton: Locator
 
     constructor(page: Page) {
         super(page)
@@ -14,17 +15,7 @@ export class HomePage extends BasePage {
         this.firstPlayForFreeButton = page.getByRole('link', { name: 'PLAY FREE' })
         this.secondPlayForFreeButton = page.locator("//*[contains(@class, 'register' )]//a").nth(2)
         this.secondPlayForFreeButton = page.locator("//*[contains(@class, 'RANDOM' )]//a")
+        this.confirmButton = page.locator("//*[contains(@class, 'Confirm' )]//a")
     }
-
-    async openHomePage() {
-        await this.page.goto('/')
-    }
-
-    async openRegisterPage() {
-        await this.firstPlayForFreeButton.waitFor() //waitFor default parameter == 'visible'
-        await this.firstPlayForFreeButton.click()
-        await this.page.waitForURL('**/auth/register')
-    }
-
     
 }
