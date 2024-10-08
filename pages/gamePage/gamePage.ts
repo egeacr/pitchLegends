9251369a588120a00f805ddafb145b6305c4800f
onurgokmenkoch <163167466+onurgokmenkoch@users.noreply.github.com>
2024-10-08 22:39:02 +0100

    game play page basics

---
+++ pages/gamePage/gamePage.ts
@@ -0,0 +1,33 @@
+import { expect, Locator, Page } from "@playwright/test";
+import BasePage from "../basePage/basePage";
+
+export class GamePage extends BasePage {
+  readonly page: Page;
+  private quitButton: Locator;
+  private endTurnButton: Locator;
+  private vsIcon: Locator;
+  private progressBar: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.quitButton = page.locator("//*[contains(@class, 'QUIT' )]//a");
+    this.endTurnButton = page.locator(
+      "xpath=/html/body/div[4]/div/div[3]/div/div[2]/div[3]/div[2]"
+    );
+    this.vsIcon = page.locator(
+      "xpath=/html/body/div[4]/div/div[1]/div/div[2]/img"
+    );
+    this.progressBar = page.locator(
+      "xpath=/html/body/div[3]/div/div[3]/div/div[2]/div[3]/div[2]/div"
+    );
+  }
+
+  async confirmGameStart() {
+    await expect(this.quitButton).toBeVisible({ timeout: 20000 });
+    await expect(this.endTurnButton).toBeVisible({ timeout: 20000 });
+    await expect(this.vsIcon).toBeVisible({ timeout: 20000 });
+    await expect(this.vsIcon).toBeVisible({ timeout: 20000 });
+  }
+
+}
--- tests/register/playWithName.spec.ts
+++ tests/register/playWithName.spec.ts
@@ -2,24 +2,14 @@ import { expect } from "../../fixtures/pomFixtures";
 import { test } from "../../fixtures/pomFixtures";
 import { fakerFunctions } from "../../utils/faker";
 
+test.describe("Register Tests", async () => {
+  test.beforeEach(async ({ homePage }) => {
+    await homePage.openHomePage();
+  });
 
-test.describe('Register Tests', async() =>{
-
-    test.beforeEach(async ({ homePage }) => {
-        await homePage.openHomePage()
-    })
-
-
-    test('Generate Random Name & Confirm', async({playPage})=> {
-        await playPage.openNamePopup(); //opens up name genrator pop up window and checks for elements visible
-        await playPage.generateRandomName(); //clicks on the name generator button and then waits for input field to be filled with text
-        await playPage.confirmRandomName();
-        
-    })
-
-
-    
-
-
-
-})
+  test("Generate Random Name & Confirm", async ({ playPage }) => {
+    await playPage.openNamePopup(); //opens up name genrator pop up window and checks for elements visible
+    await playPage.generateRandomName(); //clicks on the name generator button and then waits for input field to be filled with text
+    await playPage.confirmRandomName();
+  });
+});
