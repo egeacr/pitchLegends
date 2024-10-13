0bbd22d088173fabde4502858a7d153126e83d9c
Faruk Sevil <162142092+sevilfarukk@users.noreply.github.com>
2024-10-13 23:28:07 +0200

    button fix v3

--- pages/playPage/playPage.ts
+++ pages/playPage/playPage.ts
@@ -20,36 +20,50 @@ export class PlayPage extends BasePage {
   }
 
   async openNamePopup() {
-    var box = (await this.playButton.boundingBox())!;
-    await this.page.mouse.click(box.x + box.width / 2, box.y + box.height - 5);
+    // var box = (await this.playButton.boundingBox())!;
+    // await this.page.mouse.click(box.x + box.width / 2, box.y + box.height - 5);
     await this.page.waitForSelector('#playButton', { state: 'visible' });
+    
 
     await this.page.evaluate(() => {
-      const element = document.querySelector('.relative.flex.justify-center.items-center.p-0');
-
-      //const element2 = document.querySelector('.box-shadow:0px 0px 1000px 1000px rgba(0, 0, 0, 0.7), inset 0px 20px 30px rgba(0, 0, 0, 0.7);filter:brightness(1.5);z-index:2');
-
-
-      if (element) {
-        element.classList.remove('consistently-get-big-and-small');
-        // element2.classList.remove('consistently-get-big-and-small');
-
-      } else {
-        console.error('Element not found!');
-      }
+      const button = document.querySelector('#playButton');
+      button.style.zIndex = '9999';
+      button.style.visibility = 'visible';
+      button.style.opacity = '1';
+     // button.click();
     });
 
-    //await this.page.getByText('9Play0ArenaBURN&').press('Tab');
-    //await this.playButton.press('Enter');
-
-    // await this.playButton.dispatchEvent("click");
-
-    await expect(async () => {
-      await this.playButton.dispatchEvent("click");
-      await expect(this.page.getByText("RANDOM")).toBeVisible();
-    }).toPass({
-      intervals: [2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000],
-    });
+    /*
+        await this.page.evaluate(() => {
+          // const element = document.querySelector('.relative.flex.justify-center.items-center.p-0');
+          this.page.waitForTimeout(5000)
+    
+          const element = document.querySelector('.box-shadow:0px 0px 1000px 1000px rgba(0, 0, 0, 0.7))');
+    
+          //const element2 = document.querySelector('.box-shadow:0px 0px 1000px 1000px rgba(0, 0, 0, 0.7), inset 0px 20px 30px rgba(0, 0, 0, 0.7);filter:brightness(1.5);z-index:2');
+          //style="box-shadow:0px 0px 1000px 1000px rgba(0, 0, 0, 0.7))"
+    
+          if (element) {
+            element.classList.remove('0px 0px 1000px 1000px rgba(0, 0, 0, 0.7))');
+            // element2.classList.remove('consistently-get-big-and-small');
+    
+          } else {
+            console.error('Element not found!');
+          }
+        });
+    
+        //await this.page.getByText('9Play0ArenaBURN&').press('Tab');
+        //await this.playButton.press('Enter');
+    
+        // await this.playButton.dispatchEvent("click");
+    */
+        await expect(async () => {
+          this.page.waitForTimeout(50000)
+          await this.playButton.dispatchEvent("click");
+          await expect(this.page.getByText("RANDOM")).toBeVisible();
+        }).toPass({
+          intervals: [2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000],
+        });
   }
 
   async generateRandomName() {
