33f763b414ae5bb2883aa1d8b4125d37afd85a20
Faruk Sevil <162142092+sevilfarukk@users.noreply.github.com>
2024-10-17 22:24:12 +0200

    verify-victory-screen

--- pages/playPage/popUpVerify.ts
+++ pages/playPage/popUpVerify.ts
@@ -3,9 +3,11 @@ import { expect, Locator, Page } from "@playwright/test";
 export class PopUpVerify {
     readonly page: Page;
     readonly endTurnButton: Locator
+    readonly endGameButton: Locator
 
     constructor(page: Page) {
         this.page = page
+        this.endTurnButton = page.locator('.bg-contain.cursor-pointer.relative.bg-center.flex.justify-center.items-center.bg-no-repeat.transition-all.duration-500.ease-in-out')
         this.endTurnButton = page.locator('.bg-contain.cursor-pointer.relative.bg-center.flex.justify-center.items-center.bg-no-repeat.transition-all.duration-500.ease-in-out')
     }
 
@@ -44,6 +46,17 @@ export class PopUpVerify {
         await this.page.locator('.bg-contain > div:nth-child(3)').click({ force: true });
     }
 
+    async clickEndGameButton() {
+        await this.page.waitForTimeout(5000)
+        await this.page.locator('.bg-contain > div:nth-child(3)').click({ force: true });
+
+    }
+
+    async verifyWinZoneScreen() {
+        await this.page.waitForTimeout(2000)
+        await this.page.getByText('WIN 2 OUT OF 3 ZONES').click();
+        await this.page.waitForTimeout(2000)
+    }
     async verifyZonesInteractions() {
         const zones = ['Attack Zone', 'Midfield Zone', 'Defense Zone'];
 
@@ -52,5 +65,17 @@ export class PopUpVerify {
             await expect(zoneElement).toBeVisible();
             await zoneElement.click();
         }
+    }
+
+    async verifyVictoryScreen() {
+        await this.page.waitForTimeout(10000)
+        const imageElement = this.page.locator('div').filter({ hasText: 'You finished the tutorial!' }).locator('img');
+        await expect(imageElement).toBeVisible();
+
+        const heading = this.page.getByRole('heading', { name: 'You finished the tutorial!' });
+        await expect(heading).toBeVisible();
+
+        const textElement = this.page.getByText('Congratulations! You have successfully completed the tutorial against Julius.');
+        await expect(textElement).toBeVisible();
     }
 }
